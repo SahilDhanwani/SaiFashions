53220b981d6a36affb8a3c4044e4bba4add2a7e2
SahilDhanwani <sahildhanwani291203@gmail.com>
2024-03-22 13:47:37 +0530

    Refactor code to improve performance and readability

---
+++ main.js
@@ -0,0 +1,45 @@
+var purchase = document.getElementById('purchase');
+var percent = document.getElementById('percent');
+
+var actual = document.getElementById('actual');
+var wholesale = document.getElementById('wholesale');
+var retail = document.getElementById('retail');
+var code = document.getElementById('code');
+var no = document.getElementById('no');
+
+purchase.addEventListener('input', calculate);
+percent.addEventListener('input', calculate);
+
+var letters = ['B','G','A','R','M','E','N','T','H','U'];
+
+function calculate() {
+    var temp = parseFloat(purchase.value) + (parseFloat(purchase.value) * parseFloat(percent.value) / 100);
+    actual.textContent = temp;
+
+    if (temp % 5 <= 2)
+        temp = temp - (temp % 5);
+    else
+        temp = temp + (5 - (temp % 5));
+
+    wholesale.textContent = temp;
+    retail.textContent = temp * 2;
+    no.textContent = temp / 5;
+
+    temp = parseFloat(purchase.value);
+    var ans;
+    if(temp%1 != 0) {
+        temp = temp * 10;
+        temp = parseInt(temp);
+        ans = letters[temp%10];
+        temp = parseInt(temp/10);
+        ans = '.' + ans;
+    }
+    else 
+        ans = '';
+    temp = parseInt(temp);
+    while(temp > 0) {
+        ans = letters[temp%10] + ans;
+        temp = parseInt(temp/10);
+    }
+    code.textContent = ans;
+}
